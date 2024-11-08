d56efb7101e8e4c2a9ea871b57ddc08e7f433736
webbunivAdmin <danientege785@gmail.com>
2024-11-08 18:29:23 +0300

    commit

---
+++ src/app/publications/_components/file-browser.tsx
@@ -0,0 +1,163 @@
+"use client";
+import { useOrganization, useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { UploadButton } from "./upload-button";
+import { FileCard } from "./file-card";
+import Image from "next/image";
+import { GridIcon, Loader2, RowsIcon } from "lucide-react";
+import { SearchBar } from "./search-bar";
+import { useState } from "react";
+import { DataTable } from "./file-table";
+import { columns } from "./columns";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Doc } from "../../../../convex/_generated/dataModel";
+import { Label } from "@/components/ui/label";
+
+function Placeholder() {
+  return (
+    <div className="flex flex-col gap-8 w-full items-center mt-24">
+      <Image
+        alt="an image of a picture and directory icon"
+        width="300"
+        height="300"
+        src="/empty.svg"
+      />
+      <div className="text-2xl">You have no files, upload one now</div>
+      <UploadButton />
+    </div>
+  );
+}
+
+export function FileBrowser({
+  title,
+  favoritesOnly,
+  deletedOnly,
+}: {
+  title: string;
+  favoritesOnly?: boolean;
+  deletedOnly?: boolean;
+}) {
+  const organization = useOrganization();
+  const user = useUser();
+  const [query, setQuery] = useState("");
+  const [type, setType] = useState<Doc<"files">["type"] | "all">("all");
+
+  let orgId: string | undefined = undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+
+  const favorites = useQuery(
+    api.files.getAllFavorites,
+    orgId ? { orgId } : "skip"
+  );
+
+  const files = useQuery(
+    api.files.getFiles,
+    orgId
+      ? {
+          orgId,
+          type: type === "all" ? undefined : type,
+          query,
+          favorites: favoritesOnly,
+          deletedOnly,
+        }
+      : "skip"
+  );
+  const isLoading = files === undefined;
+
+  const modifiedFiles =
+    files?.map((file) => ({
+      ...file,
+      isFavorited: (favorites ?? []).some(
+        (favorite) => favorite.fileId === file._id
+      ),
+    })) ?? [];
+
+  return (
+    <div>
+      <div className="hidden md:flex justify-between items-center mb-8">
+        <h1 className="text-4xl font-bold">{title}</h1>
+
+        <SearchBar query={query} setQuery={setQuery} />
+
+        <UploadButton />
+      </div>
+      <div className="md:hidden flex flex-col gap-5 mb-8">
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <UploadButton />
+
+        <SearchBar query={query} setQuery={setQuery} />
+
+      </div>
+
+      <Tabs defaultValue="grid">
+        <div className="flex flex-col-reverse gap-4 md:gap-0 md:flex-row md:justify-between md:items-center items-start">
+          <TabsList className="mb-2">
+            <TabsTrigger value="grid" className="flex gap-2 items-center">
+              <GridIcon />
+              Grid
+            </TabsTrigger>
+            <TabsTrigger value="table" className="flex gap-2 items-center">
+              <RowsIcon /> Table
+            </TabsTrigger>
+          </TabsList>
+
+          <div className="flex gap-2 items-center">
+            <Label htmlFor="type-select">Type Filter</Label>
+            <Select
+              value={type}
+              onValueChange={(newType) => {
+                setType(newType as any);
+              }}
+            >
+              <SelectTrigger id="type-select" className="w-[180px]">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                <SelectItem value="image">Image</SelectItem>
+                <SelectItem value="csv">CSV</SelectItem>
+                <SelectItem value="pdf">PDF</SelectItem>
+                <SelectItem value="ppt">PPT</SelectItem>
+                <SelectItem value="pptx">PPTX</SelectItem>
+                <SelectItem value="doc">DOCS</SelectItem>
+                <SelectItem value="docx">DOCX</SelectItem>
+                <SelectItem value="xlsx">EXCEL</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
+        {isLoading && (
+          <div className="flex flex-col gap-8 w-full items-center mt-12 md:mt-24">
+            <Loader2 className="h-32 w-32 animate-spin text-gray-500" />
+            <div className="text-2xl">Loading your files...</div>
+          </div>
+        )}
+
+        <TabsContent value="grid">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-4">
+            {modifiedFiles?.map((file) => {
+              return <FileCard key={file._id} file={file} />;
+            })}
+          </div>
+        </TabsContent>
+        <TabsContent value="table">
+          {/* @ts-ignore */}
+          <DataTable columns={columns} data={modifiedFiles} />
+        </TabsContent>
+      </Tabs>
+
+      {files?.length === 0 && <Placeholder />}
+    </div>
+  );
+}
